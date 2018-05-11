92d827119ee7dc66046b76b1069bd7bdc49bd01e
throwawaysendtits <39202622+throwawaysendtits@users.noreply.github.com>
2018-05-11 15:45:36 -0500

    Create Reddit Random Button.user.js

---
+++ Reddit Random Button.user.js
@@ -0,0 +1,13 @@
+// ==UserScript==
+// @name       Reddit Random Buttons
+// @version    0.1
+// @description  Add random buttons
+// @match      https://www.reddit.com/*
+// @author     everyone
+// @require http://code.jquery.com/jquery-latest.js
+// ==/UserScript==
+
+$(document).ready(function() {
+   var rand = document.getElementById('header');
+  $(rand).append('<a href="http://www.reddit.com/r/random">random</a>&nbsp;&nbsp;');
+});
