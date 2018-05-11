09cabfb8ae98ef66f746d1aa267cb5699cb947f6
throwawaysendtits <39202622+throwawaysendtits@users.noreply.github.com>
2018-05-11 15:47:36 -0500

    Create randnsfw.user.js

---
+++ randnsfw.user.js
@@ -0,0 +1,13 @@
+// ==UserScript==
+// @name       Reddit randnsfw button
+// @version    0.1
+// @description  Add randnsfw button
+// @match      https://www.reddit.com/*
+// @author     everyone
+// @require http://code.jquery.com/jquery-latest.js
+// ==/UserScript==
+
+$(document).ready(function() {
+   var rand = document.getElementById('header');
+  $(rand).append('<a href="http://www.reddit.com/r/randnsfw">randnsfw</a>&nbsp;&nbsp;');
+});
